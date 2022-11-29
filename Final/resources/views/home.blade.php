765a5018c177799dd0969ba637464e0e6ba38dfb
Reyes <jreyes202@stevenscollege.edu>
2022-11-29 15:13:00 -0500

    Added Layout for Home Page and Register Page. Added routed for both pages

--- Final/resources/views/home.blade.php
+++ Final/resources/views/home.blade.php
@@ -1,1 +1,10 @@
+<html>
+    <h1>Home Page</h1>
 
+    <form action="login" method="get">
+        <input type="submit" value="Login">
+    </form>
+    <form action="register" method="get">
+        <input type="submit" value="Signup">
+    </form>
+</html>
--- Final/routes/web.php
+++ Final/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', [WebController::class, 'login_view']);
+
+Route::get('/register', [WebController::class, 'register_view']);
