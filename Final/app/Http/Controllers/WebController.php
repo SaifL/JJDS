c40721cd645bc001e9cd6705b02f63d7fbad2a3d
Reyes <jreyes202@stevenscollege.edu>
2022-12-01 12:32:48 -0500

    changed all routes to WebController and added connections in web.php

--- Final/app/Http/Controllers/WebController.php
+++ Final/app/Http/Controllers/WebController.php
@@ -7,12 +7,26 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
+
+    public function home_view(){
+        return view('home');
+    }
+
+
     public function login_view(){
         return view('login');
     }
 
     public function register_view(){
         return view('register');
+    }
+
+    public function patient_view(){
+        return view('patienthome');
+    }
+
+    public function doctor_view(){
+        return view('doctorhome');
     }
 
 
