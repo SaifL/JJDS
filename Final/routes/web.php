1b4e9f49e85df1ab3b8fe320b4edd049c69d47b3
Reyes <jreyes202@stevenscollege.edu>
2022-12-02 13:12:48 -0500

    Login function takes all roles to proper home page. Admin and supervisor home pages have been added and have links to all pages they can access

--- Final/routes/web.php
+++ Final/routes/web.php
@@ -22,7 +22,6 @@ Route::get('/login', [WebController::class, 'login_view']);
 
 Route::get('/register', [WebController::class, 'register_view']);
 
-
 Route::get('/phome', [WebController::class, 'patient_view']);
 
 Route::get('/dhome', [WebController::class, 'doctor_view']);
@@ -30,3 +29,27 @@ Route::get('/dhome', [WebController::class, 'doctor_view']);
 Route::get('/fhome', [WebController::class, 'family_home']);
 
 Route::get('/chome', [WebController::class, 'caregiver_home']);
+
+Route::get('/ahome', [WebController::class, 'admin_home']);
+
+Route::get('/shome', [WebController::class, 'supervisor_home']);
+
+Route::get('/info', [WebController::class, 'add_info']);
+
+Route::get('/appointment', [WebController::class, 'doc_appoint']);
+
+Route::get('/role', [WebController::class, 'role_view']);
+
+Route::get('/employee', [WebController::class, 'employee']);
+
+Route::get('/patients', [WebController::class, 'patients']);
+
+Route::get('/approval', [WebController::class, 'reg_approval']);
+
+Route::get('/roster', [WebController::class, 'roster_view']);
+
+Route::get('/makeroster', [WebController::class, 'new_roster']);
+
+Route::get('/report', [WebController::class, 'admin_report']);
+
+Route::get('/payment', [WebController::class, 'payment']);
