5a7fec7cabe9cedff661ec352ac370d3af4e462b
Yarborough <dmyarborough202@stevenscollege.edu>
2022-12-02 13:47:03 -0500

    added a testcase for testing stuff in the future. (Will delete it after testing stuff but its causing issues currently)

--- Final/routes/api.php
+++ Final/routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/role_login', [MainController::class, 'role_login']);
 
 Route::post('/register', [MainController::class, 'register']);
+
+Route::get('/testcase', [MainController::class, 'testcase']);
