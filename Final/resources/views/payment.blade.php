f0af9b63e1f31ad02de6b8219c87e44fcaa92abd
Javlon456 <lonjav456@gmail.com>
2022-12-05 15:20:25 -0500

    Payment page and Admin Report page layout done.

---
+++ Final/resources/views/payment.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <form action="">
+        <label>Patient ID: </label>
+        <input type="text">
+        <label>Total Due: </label>
+        <input type="text">
+        <label>New Payment</label>
+        <input type="text"><br>
+        <input type="submit" value='Ok'>
+    </form>
+    <form action="">
+        <input type="submit" value="Cancel">
+    </form>
+</html>
