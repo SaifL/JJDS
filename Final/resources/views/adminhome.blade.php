b6a0bb38dde15b099ddb5870ebfffaa526ba4de6
SaifL <leach_saif@yahoo.com>
2022-12-09 13:26:05 -0500

    sadf

--- Final/resources/views/adminhome.blade.php
+++ Final/resources/views/adminhome.blade.php
@@ -1,43 +1,43 @@
 <html>
     <h1>Admin Home</h1>
-
+    <header><link rel="stylesheet" href="{{ asset('css/roster.css') }}"></header>
     <form action="/info" method="get">
-        <input type="submit" value="Addition Patient Info">
+        <input class="where" type="submit" value="Addition Patient Info">
     </form>
 
     <form action="/appointment" method="get">
-        <input type="submit" value="Doctor's Appointments">
+        <input class="where" type="submit" value="Doctor's Appointments">
     </form>
 
     <form action="/role" method="get">
-        <input type="submit" value="Roles">
+        <input class="where" type="submit" value="Roles">
     </form>
 
     <form action="/employee" method="get">
-        <input type="submit" value="Employee">
+        <input class="where" type="submit" value="Employee">
     </form>
 
     <form action="/patients" method="get">
-        <input type="submit" value="Patients">
+        <input class="where" type="submit" value="Patients">
     </form>
 
     <form action="/approval" method="get">
-        <input type="submit" value="Approval">
+        <input class="where" type="submit" value="Approval">
     </form>
 
     <form action="/roster" method="get">
-        <input type="submit" value="Roster">
+        <input class="where" type="submit" value="Roster">
     </form>
 
     <form action="/makeroster" method="get">
-        <input type="submit" value="Make New Roster">
+        <input class="where" type="submit" value="Make New Roster">
     </form>
 
     <form action="/report" method="get">
-        <input type="submit" value="View Report">
+        <input class="where" type="submit" value="View Report">
     </form>
     
     <form action="/payment" method="get">
-        <input type="submit" value="Payments">
+        <input class="where" type="submit" value="Payments">
     </form>
 </html>
