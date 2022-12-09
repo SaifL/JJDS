7223057123b63efcbcf1f3dd7fe99e697d447a45
SaifL <leach_saif@yahoo.com>
2022-12-09 14:41:15 -0500

    no

--- Final/resources/views/supervisorhome.blade.php
+++ Final/resources/views/supervisorhome.blade.php
@@ -1,31 +1,31 @@
 <html>
     <h1>Supervisor Home</h1>
-
+    <header><link rel="stylesheet" href="{{ asset('css/approval.css') }}"></header>
     <form action="/info" method="get">
-        <input type="submit" value="Addition Patient Info">
+        <input class="where2" type="submit" value="Addition Patient Info">
     </form>
 
     <form action="/employee" method="get">
-        <input type="submit" value="Employee">
+        <input class="where2" type="submit" value="Employee">
     </form>
 
     <form action="/patients" method="get">
-        <input type="submit" value="Patients">
+        <input class="where2" type="submit" value="Patients">
     </form>
 
     <form action="/approval" method="get">
-        <input type="submit" value="Approval">
+        <input class="where2" type="submit" value="Approval">
     </form>
 
     <form action="/roster" method="get">
-        <input type="submit" value="Roster">
+        <input class="where2" type="submit" value="Roster">
     </form>
 
     <form action="/makeroster" method="get">
-        <input type="submit" value="Make New Roster">
+        <input class="where2" type="submit" value="Make New Roster">
     </form>
 
     <form action="/report" method="get">
-        <input type="submit" value="View Report">
+        <input class="where2" type="submit" value="View Report">
     </form>
 </html>
